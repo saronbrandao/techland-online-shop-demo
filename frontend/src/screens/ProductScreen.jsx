1b52c70f3ff03f297b73594e082bd783d0c1b68d
Saron <saronbrandao@gmail.com>
2023-07-01 18:38:24 +0200

    Fetching products from the server

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,23 @@
 import { useParams } from 'react-router-dom';
-import products from '../products';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+// import products from '../products';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState([]);
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [productId]);
 
   return (
     <>

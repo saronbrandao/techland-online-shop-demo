04ae59986125d1d48fdd84b0a4b0d7b50dc75ecf
Saron <saronbrandao@gmail.com>
2023-08-03 00:41:14 +0200

    updates

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,15 @@ import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
+const __dirname = path.resolve(); // Set __dirname to current directory
+
 
 // Connect to MongoDB
 connectDB();
 const app = express();
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
 
 // Body parser middleware
 app.use(express.json());
@@ -27,9 +32,7 @@ app.use(
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -41,8 +44,20 @@ app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-const __dirname = path.resolve(); // Set __dirname to current directory
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+
+if(process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  // any route that is not api will be redirected to index.html
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -11,7 +11,7 @@ const ProductCarousel = () => {
   return isLoading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : <Carousel pause='hover' className='bg-primary mb-4'>
     {products.map(product => (
       <Carousel.Item key={product._id}>
-        <Link to={`/product/${product._is}`}>
+        <Link to={`/product/${product._id}`}>
           <Image src={product.image} alt={product.name} fluid />
           <Carousel.Caption>
             <h2>{product.name} ${product.price}</h2>

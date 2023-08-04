0237d2fe1d4f77a43dc87bcceb2fbbd148541c92
Saron <saronbrandao@gmail.com>
2023-08-04 23:00:04 +0200

    updates

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
+import dotenv from 'dotenv';
 dotenv.config();
 import path from 'path';
 import express from 'express';
-import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
@@ -13,13 +13,11 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000;
 const __dirname = path.resolve(); // Set __dirname to current directory
 
-
 // Connect to MongoDB
 connectDB();
 const app = express();
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
 
 // Body parser middleware
 app.use(express.json());
@@ -32,8 +30,6 @@ app.use(
 // Cookie parser middleware
 app.use(cookieParser());
 
-
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -44,14 +40,14 @@ app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-
-
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // set static folder
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   // any route that is not api will be redirected to index.html
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
 } else {
   app.get('/', (req, res) => {
     res.send('API is running...');
--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -21,7 +21,7 @@ const authSlice = createSlice({
     },
   },
 });
-
+// 
 export const { setCredentials, logout } = authSlice.actions;
 
 export default authSlice.reducer;

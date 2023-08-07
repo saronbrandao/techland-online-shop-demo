55bb8a46a3d79467f95361451013d82b9c4d481d
Saron <saronbrandao@gmail.com>
2023-08-07 23:13:30 +0200

    updates

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,20 +8,23 @@ const getProducts = asyncHandler(async (req, res) => {
   const pageSize = process.env.PAGINATION_LIMIT;
   const page = Number(req.query.pageNumber) || 1;
 
-  const keyword = req.query.keyword ? {
-    name: {
-      $regex: req.query.keyword,
-      $options: 'i'
-    }
-  } : {}
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: 'i',
+        },
+      }
+    : {};
 
-
-  const count = await Product.countDocuments({...keyword})
-  const products = await Product.find({...keyword}).limit(pageSize).skip(pageSize * (page - 1));
+  const count = await Product.countDocuments({ ...keyword });
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
   res.json({
     products,
     page,
-    pages: Math.ceil(count / pageSize)
+    pages: Math.ceil(count / pageSize),
   });
 });
 
@@ -49,7 +52,7 @@ const createProduct = asyncHandler(async (req, res) => {
     name: 'Sample name',
     price: 0,
     user: req.user._id,
-    image: 'images/sample.jpg',
+    image: '/images/sample.jpg',
     brand: 'Sample brand',
     category: 'Sample category',
     countInStock: 0,
@@ -115,40 +118,41 @@ const deleteProduct = asyncHandler(async (req, res) => {
 // @route     POST /api/products/:id/reviews
 // @access    Private
 const createProductReview = asyncHandler(async (req, res) => {
-  const {rating, comment} = req.body;
+  const { rating, comment } = req.body;
 
   const product = await Product.findById(req.params.id);
 
   if (product) {
     const alreadyReviewed = product.reviews.find(
-      review => review.user.toString() === req.user._id.toString()
-    )    
+      (review) => review.user.toString() === req.user._id.toString()
+    );
 
-    if(alreadyReviewed) { 
+    if (alreadyReviewed) {
       res.status(400);
-      throw new Error('Product already reviewed')
+      throw new Error('Product already reviewed');
     }
 
     const review = {
       name: req.user.name,
       rating: Number(rating),
       comment,
-      user: req.user._id
-    }
+      user: req.user._id,
+    };
 
     product.reviews.push(review);
 
     product.numReviews = product.reviews.length;
 
-    product.rating = product.reviews.reduce((acc, review) => acc + review.rating, 0) / product.reviews.length;
+    product.rating =
+      product.reviews.reduce((acc, review) => acc + review.rating, 0) /
+      product.reviews.length;
 
     await product.save();
 
-    res.status(201).json({message: 'Review added'})
-
-  }  else {
+    res.status(201).json({ message: 'Review added' });
+  } else {
     res.status(404);
-    throw new Error('Resource not found')
+    throw new Error('Resource not found');
   }
 });
 
@@ -156,7 +160,7 @@ const createProductReview = asyncHandler(async (req, res) => {
 // @route     GET /api/products/top
 // @access    Public
 const getTopProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({}).sort({rating: -1}).limit(3);
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
   res.status(200).json(products);
 });
 
@@ -167,5 +171,5 @@ export {
   updateProduct,
   deleteProduct,
   createProductReview,
-  getTopProducts
+  getTopProducts,
 };
